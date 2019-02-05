7156638d4600794e45b25bd9e9dbc5520beecea0
gsanchezz <gsanchez@conectium.com>
2019-02-05 16:37:43 -0400

    Agregando comentarios a Filtrado

--- 05_Browser/02_ToDo/todo.js
+++ 05_Browser/02_ToDo/todo.js
@@ -14,12 +14,14 @@ const filters = {
 }
 
 const renderNotes = function (notes, filters) {
+    //FILTRANDO A PARTIR DE MI ARRAY
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
 
     document.querySelector('#notes').innerHTML = ''
     
+    //AGREGANDO A PAGINA LAS NOTAS QUE PASARON EL FILTRO
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
         noteEl.textContent = note.title
@@ -27,12 +29,15 @@ const renderNotes = function (notes, filters) {
     })
 }
 
+//MOSTRANDO AL MENOS ALGO EN LA PAGINA
 renderNotes(notes, filters)
 
+//CUANDO SE TOCA EL BOTON DE CREAR SOLO DICE QUE FUE TOCADO
 document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'
 })
 
+//CADA VEZ QUE EL INPUT DE SEARCH TEXT CAMBIA SE LLAMA A LA FUNCION "renderNotes"
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
