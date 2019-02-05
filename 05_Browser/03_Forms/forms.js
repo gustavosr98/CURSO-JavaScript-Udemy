e068763866593898af91190662b280e351d97d77
gsanchezz <gsanchez@conectium.com>
2019-02-05 17:47:11 -0400

    Form Challenge 03_Forms

---
+++ 05_Browser/03_Forms/forms.js
@@ -0,0 +1,61 @@
+const notes = [
+    {
+        titulo:'Aguacate',
+        date: 1900
+    },
+    {
+        titulo:'Fresa',
+        date: 1970
+    },
+    {
+        titulo:'Patilla',
+        date: 2019
+    }
+]
+
+console.log(notes)
+
+const renderNotes = function( notes, searchText ){
+    //FILTRANDO A PARTIR DE MI ARRAY
+    const filteredNotes = notes.filter( function(item){
+        return item.titulo.toLowerCase().includes( searchText.toLowerCase() )
+    })
+    
+    //LIMPIO DIV NOTES    
+    document.querySelector('#notes').innerHTML = ''
+
+    //AGREGANDO A PAGINA LAS NOTAS QUE PASARON EL FILTRO
+    filteredNotes.forEach( function(item) {
+        const tmpP = document.createElement('p')
+        tmpP.textContent = item.titulo
+        document.querySelector('#notes').appendChild(tmpP)
+    })
+
+}
+
+//MOSTRANDO AL MENOS ALGO EN LA PAGINA
+renderNotes(notes, '')
+
+//CADA VEZ QUE EL INPUT DE SEARCH TEXT CAMBIA SE LLAMA A LA FUNCION "renderNotes"
+document.querySelector('#buscador').addEventListener('input', function(e){
+    renderNotes( notes, e.target.value )
+})
+
+
+//AGREGAR ELEMENTO AL ARRAY
+document.querySelector('#formulario1').addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    console.log(e.target.elements.titulo)
+
+    const newTitulo = e.target.elements.titulo.value
+    const newFecha = e.target.elements.fecha.value
+    notes.push( { titulo: newTitulo, date: newFecha } )
+
+    e.target.elements.titulo.value = ''
+    e.target.elements.fecha.value = null
+
+    renderNotes(notes, '')
+})
+
+
