017b51b96f79b128b05e4b03266b5ff62210b60d
gsanchezz <gsanchez@conectium.com>
2019-02-06 10:57:33 -0400

    Deletes

--- 06_Data_Storage/01_Local_Storage/forms.js
+++ 06_Data_Storage/01_Local_Storage/forms.js
@@ -1,19 +1,30 @@
 const notes = [
     {
+        id: 0,
         titulo:'Aguacate',
         date: 1900
     },
     {
+        id: 1,
         titulo:'Fresa',
         date: 1970
     },
     {
+        id: 2,
         titulo:'Patilla',
         date: 2019
     }
 ]
 
-console.log(notes)
+const deleteById = function(id){
+    const i = notes.findIndex( function(item){
+        return item.id === id
+    })
+
+    if (i > -1)
+        notes.splice(i, 1)
+    
+} 
 
 const renderNotes = function( notes, searchText ){
     //FILTRANDO A PARTIR DE MI ARRAY
@@ -26,9 +37,27 @@ const renderNotes = function( notes, searchText ){
 
     //AGREGANDO A PAGINA LAS NOTAS QUE PASARON EL FILTRO
     filteredNotes.forEach( function(item) {
-        const tmpP = document.createElement('p')
-        tmpP.textContent = item.titulo
-        document.querySelector('#notes').appendChild(tmpP)
+
+        //CREAR <div>
+        const div = document.createElement('div')
+        //CREAR <button>
+        const button = document.createElement('button')
+        button.textContent = 'x'
+        button.addEventListener('click', function(){
+            deleteById(item.id)
+            renderNotes(notes, '')
+        })
+
+        //CREAR <span>
+        const span = document.createElement('span')
+        span.textContent = item.titulo
+
+        //APPENDING
+        div.appendChild(button)
+        div.appendChild(span)
+
+        document.querySelector('#notes').appendChild(div)
+
     })
 
 }
@@ -38,12 +67,12 @@ document.querySelector('#buscador').addEventListener('input', function(e){
     renderNotes( notes, e.target.value )
 })
 
-
 //AGREGAR ELEMENTO AL ARRAY
 document.querySelector('#formulario1').addEventListener('submit', function(e) {
     e.preventDefault()
 
     const newNote = {
+        id: uuidv4(),
         titulo: e.target.elements.titulo.value, 
         date: e.target.elements.fecha.value
     }
