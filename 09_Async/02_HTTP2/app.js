7e22843d96723bae21e3ca9a94bc93a20aa5af82
gsanchezz <gsanchez@conectium.com>
2019-02-07 10:02:10 -0400

    Pequenos cambios

--- 09_Async/02_HTTP2/app.js
+++ 09_Async/02_HTTP2/app.js
@@ -2,8 +2,9 @@ const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
     if( e.target.readyState === 4){
-        let pais = buscarPais( JSON.parse( e.target.responseText ) , 'VEN' )
+        const pais = buscarPais( JSON.parse( e.target.responseText ) , 'AFG' )
         console.log(JSON.stringify(pais, null, 4))
+        mostrarPais( pais )
     } else {
         console.log(e.target)
     }
--- 09_Async/02_HTTP2/funciones.js
+++ 09_Async/02_HTTP2/funciones.js
@@ -1,3 +1,7 @@
 const buscarPais = function (paisesArray, code3pais) {
     return paisesArray.find( (pais) => pais.alpha3Code === code3pais) 
 }
+
+const mostrarPais = function(pais){
+    document.querySelector('#pais').textContent = `${pais.name} (${pais.alpha3Code}) tiene como capital ${pais.capital}`
+}
